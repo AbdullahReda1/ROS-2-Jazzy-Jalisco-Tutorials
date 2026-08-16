68872af26fdcdb8b2df1061f81f74bd2fef2624d
Abdullah <abdullah.reda44@gmail.com>
2026-08-17 02:53:02 +0300

    Refactor add_two_ints client and server implementations for clarity and consistency

--- 01-Beginner-Client-libraries/05-Writing-a-simple-service-and-client-CPP/ros2_ws/src/cpp_srvcli/src/add_two_ints_client.cpp
+++ 01-Beginner-Client-libraries/05-Writing-a-simple-service-and-client-CPP/ros2_ws/src/cpp_srvcli/src/add_two_ints_client.cpp
@@ -1,36 +1,47 @@
-#include <rclcpp/rclcpp.hpp>
-#include <example_interfaces/srv/add_two_ints.hpp>
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/srv/add_two_ints.hpp"
 
-#include <memory>
 #include <chrono>
 #include <cstdlib>
+#include <memory>
 
 using namespace std::chrono_literals;
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     rclcpp::init(argc, argv);
+
     if (argc != 3) {
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Use 2 input cmd parameters for adding X Y");
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: add_two_ints_client X Y");
         return 1;
     }
+
     std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_client");
-    rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr cleint = node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
+    rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client =
+        node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
+
     auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
     request->a = atoll(argv[1]);
     request->b = atoll(argv[2]);
-    while (!cleint->wait_for_service(1s)) {
+
+    while (!client->wait_for_service(1s)) {
         if (!rclcpp::ok()) {
-            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "The client was interrupted while waiting the service. Exit");
+            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
             return 0;
         }
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "The cleint will wait again for service");
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
     }
-    auto result = cleint->async_send_request(request);
-    if (rclcpp::spin_until_future_complete(node, result) == rclcpp::FutureReturnCode::SUCCESS)
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sum: %ld", result.get()->sum);
-    else
-        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service");
+
+    auto result = client->async_send_request(request);
+    // Wait for the result.
+    if (rclcpp::spin_until_future_complete(node, result) ==
+        rclcpp::FutureReturnCode::SUCCESS)
+    {
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %ld", result.get()->sum);
+    } else {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
+    }
+
     rclcpp::shutdown();
     return 0;
 }
